fc34af6343f9cad3f52c2e5bde3975678d783a06
Striveonger <striveonger@163.com>
2022-11-03 08:06:03 +0800

    规范公共组价命名

---
+++ data-hamal-auth/src/main/java/com/striveonger/study/auth/entity/Users.java
@@ -0,0 +1,159 @@
+package com.striveonger.study.auth.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * <p>
+ * 用户表
+ * </p>
+ *
+ * @author Mr.Lee
+ * @since 2022-10-30
+ */
+@ApiModel(value = "Users对象", description = "用户表")
+public class Users implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty("用户ID")
+    private String id;
+
+    @ApiModelProperty("用户账号")
+    private String username;
+
+    @ApiModelProperty("密码")
+    private String password;
+
+    @ApiModelProperty("用户昵称")
+    private String nickname;
+
+    @ApiModelProperty("用户邮箱")
+    private String email;
+
+    @ApiModelProperty("手机号码")
+    private String telNumber;
+
+    @ApiModelProperty("用户性别（1男 2女 0未知）")
+    private Boolean sex;
+
+    @ApiModelProperty("头像地址")
+    private String avatar;
+
+    @ApiModelProperty("帐号状态（0正常 1停用）")
+    private Boolean status;
+
+    @ApiModelProperty("创建时间")
+    private LocalDateTime createTime;
+
+    @ApiModelProperty("更新时间")
+    private LocalDateTime updateTime;
+
+    @ApiModelProperty("删除标志（0否 1是）")
+    private Boolean deleted;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getTelNumber() {
+        return telNumber;
+    }
+
+    public void setTelNumber(String telNumber) {
+        this.telNumber = telNumber;
+    }
+    public Boolean getSex() {
+        return sex;
+    }
+
+    public void setSex(Boolean sex) {
+        this.sex = sex;
+    }
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+    public LocalDateTime getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(LocalDateTime updateTime) {
+        this.updateTime = updateTime;
+    }
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+            "id=" + id +
+            ", username=" + username +
+            ", password=" + password +
+            ", nickname=" + nickname +
+            ", email=" + email +
+            ", telNumber=" + telNumber +
+            ", sex=" + sex +
+            ", avatar=" + avatar +
+            ", status=" + status +
+            ", createTime=" + createTime +
+            ", updateTime=" + updateTime +
+            ", deleted=" + deleted +
+        "}";
+    }
+}
---
+++ data-hamal-commons/data-hamal-commons-mybatis/src/main/java/com/striveonger/study/mybatis/config/MybatisConfig.java
@@ -0,0 +1,34 @@
+package com.striveonger.study.mybatis.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Mr.Lee
+ * @description:
+ * @date 2022-11-02 21:26
+ */
+@Configuration
+public class MybatisConfig {
+    private final Logger log = LoggerFactory.getLogger(MybatisConfig.class);
+
+    /**
+     * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题(该属性会在旧插件移除后一同移除)
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+
+
+
+
+}
