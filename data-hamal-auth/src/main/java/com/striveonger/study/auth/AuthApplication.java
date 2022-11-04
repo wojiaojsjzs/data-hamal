b3a40f67e078756e3fb24dc40ffe2d5049396461
Striveonger <striveonger@163.com>
2022-11-04 19:02:09 +0800

    调整格式

--- data-hamal-auth/src/main/java/com/striveonger/study/auth/AuthApplication.java
+++ data-hamal-auth/src/main/java/com/striveonger/study/auth/AuthApplication.java
@@ -1,5 +1,7 @@
 package com.striveonger.study.auth;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
@@ -11,10 +13,10 @@ import org.springframework.boot.context.metrics.buffering.BufferingApplicationSt
  */
 @SpringBootApplication
 public class AuthApplication {
+    private static final Logger log = LoggerFactory.getLogger(AuthApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  认证授权中心启动成功   ლ(´ڡ`ლ)ﾞ");
+        log.info("(♥◠‿◠)ﾉﾞ  data-hamal-auth start success  ლ(´ڡ`ლ)ﾞ");
     }
-
 }
--- data-hamal-commons/data-hamal-commons-mybatis/src/main/java/com/striveonger/study/mybatis/config/MybatisConfig.java
+++ data-hamal-commons/data-hamal-commons-mybatis/src/main/java/com/striveonger/study/mybatis/config/MybatisConfig.java
@@ -7,12 +7,12 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.mybatis.spring.mapper.MapperScannerConfigurer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.Resource;
@@ -29,7 +29,7 @@ public class MybatisConfig {
     private final Logger log = LoggerFactory.getLogger(MybatisConfig.class);
 
     @Value("${data.hamal.dao.mapper.package}")
-    private String basePackage = "com.striveonger.study.auth.mapper";
+    private String basePackage;
 
     /**
      * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题(该属性会在旧插件移除后一同移除)
