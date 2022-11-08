bd9f89a4a59aea4a2b5a85e6c2bf46614556526b
Striveonger <striveonger@163.com>
2022-11-09 07:36:34 +0800

    补全计算文件Hash的工具类 & 增加单元测试

--- data-hamal-commons/data-hamal-commons-tools/src/main/java/com/striveonger/study/tools/FileHash.java
+++ data-hamal-commons/data-hamal-commons-tools/src/main/java/com/striveonger/study/tools/FileHash.java
@@ -3,14 +3,53 @@ package com.striveonger.study.tools;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * @author Mr.Lee
  * @description:
  * @date 2022-11-08 21:32
  */
-public class FileHash {
-    private final Logger log = LoggerFactory.getLogger(FileHash.class);
+public enum FileHash {
 
+    MD5("MD5"), SHA1("SHA1"), SHA256("SHA-256"), SHA512("SHA-512");
 
+    private final Logger log = LoggerFactory.getLogger(SpringContextHolder.class);
+    private final String name;
+
+    FileHash(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String code(File file) {
+        try (InputStream input = new FileInputStream(file)) {
+            MessageDigest digest = MessageDigest.getInstance(getName());
+            byte[] bytes = new byte[4096];
+            int count;
+            while ((count = input.read(bytes)) > 0) {
+                digest.update(bytes, 0, count);
+            }
+            bytes = digest.digest();
+            StringBuilder code = new StringBuilder();
+            for (int i = 0; i < bytes.length; i++) {
+                code.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            return code.toString();
+        } catch (FileNotFoundException e) {
+            log.error("读取文件失败", e);
+        } catch (IOException e) {
+            log.error("文件流异常", e);
+        } catch (NoSuchAlgorithmException e) {
+            log.error("未生成文件摘要", e);
+        }
+        return null;
+    }
 
 }
---
+++ data-hamal-commons/data-hamal-commons-tools/src/test/java/com/striveonger/study/tools/FileHashTest.java
@@ -0,0 +1,22 @@
+package com.striveonger.study.tools;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+public class FileHashTest {
+
+    @Test
+    public void testFileHash() {
+        String path1 = "/Users/striveonger/tmp/app1.sh";
+        String path2 = "/Users/striveonger/tmp/app2.sh";
+        File file1 = new File(path1), file2 = new File(path2);
+        String code1 = FileHash.SHA512.code(file1);
+        String code2 = FileHash.SHA512.code(file2);
+        Assertions.assertEquals(code1, code2);
+        System.out.println(code1);
+        System.out.println(code2);
+
+    }
+}
