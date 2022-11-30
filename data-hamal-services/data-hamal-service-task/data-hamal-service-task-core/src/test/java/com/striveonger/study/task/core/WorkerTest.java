337bb01b5031225fc16be4bde8cff910f5233a76
Striveonger <striveonger@163.com>
2022-11-30 22:15:43 +0800

    Task 持续更新～

--- data-hamal-services/data-hamal-service-task/data-hamal-service-task-core/src/test/java/com/striveonger/study/task/core/WorkerTest.java
+++ data-hamal-services/data-hamal-service-task/data-hamal-service-task-core/src/test/java/com/striveonger/study/task/core/WorkerTest.java
@@ -7,8 +7,10 @@ import com.striveonger.study.task.core.flow.SerialFlow;
 import com.striveonger.study.tools.SleepHelper;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.Consumer;
 
 /**
  * @author Mr.Lee
@@ -42,62 +44,70 @@ public class WorkerTest {
         WorkArea workArea = new WorkArea(1L);
         WorkArea.Worker worker = workArea.getWorker();
 
-        int waitTime = 3;
-        Executable A = () -> {
-            System.out.println("A start " + DateUtil.now());
-            SleepHelper.sleepSeconds(1);
-            System.out.println("A end " + DateUtil.now());
+        Consumer<String> consumer = new Consumer<String>() {
+            int waitTime = 1;
+            @Override
+            public void accept(String s) {
+                System.out.println(s + " start " + DateUtil.now());
+                SleepHelper.sleepSeconds(waitTime);
+                System.out.println(s + " end " + DateUtil.now());
+            }
         };
-        Executable B = () -> {
-            System.out.println("B start " + DateUtil.now());
-            SleepHelper.sleepSeconds(2);
-            System.out.println("B end " + DateUtil.now());
-        };
-        Executable C = () -> {
-            SleepHelper.sleepSeconds(waitTime);
-            System.out.println("C");
-        };
-        Executable D = () -> {
-            SleepHelper.sleepSeconds(waitTime);
-            System.out.println("D");
-        };
-        Executable E = () -> {
-            SleepHelper.sleepSeconds(waitTime);
-            System.out.println("E");
-        };
-        Executable F = () -> {
-            SleepHelper.sleepSeconds(waitTime);
-            System.out.println("F");
-        };
-        Executable G = () -> {
-            SleepHelper.sleepSeconds(waitTime);
-            System.out.println("G");
-        };
-        Executable H = () -> {
-            SleepHelper.sleepSeconds(waitTime);
-            System.out.println("H");
-        };
-        Executable I = () -> {
-            SleepHelper.sleepSeconds(waitTime);
-            System.out.println("I");
-        };
-        Executable J = () -> {
-            SleepHelper.sleepSeconds(waitTime);
-            System.out.println("J");
-        };
+
+        Executable A = () -> consumer.accept("A");
+        Executable B = () -> consumer.accept("B");
+        Executable C = () -> consumer.accept("C");
+        Executable D = () -> consumer.accept("D");
+        Executable E = () -> consumer.accept("E");
+        Executable F = () -> consumer.accept("F");
+        Executable G = () -> consumer.accept("G");
+        Executable H = () -> consumer.accept("H");
+        Executable I = () -> consumer.accept("I");
+        Executable J = () -> consumer.accept("J");
 
         SerialFlow full = new SerialFlow();
-        // A, B =>
-        // SerialFlow flow_1 = new SerialFlow();
-        ParalleFlow flow_1 = new ParalleFlow();
-        flow_1.setWorkArea(workArea);
-        flow_1.addTask(A);
-        flow_1.addTask(B);
+        full.setWorkArea(workArea);
+
+        ParalleFlow AB_P = new ParalleFlow();
+        AB_P.setWorkArea(workArea);
+        AB_P.addTask(A);
+        AB_P.addTask(B);
+
+        SerialFlow ABC_S = new SerialFlow();
+        ABC_S.setWorkArea(workArea);
+        ABC_S.addTask(AB_P);
+        ABC_S.addTask(C);
+
+        SerialFlow FH_S = new SerialFlow();
+        FH_S.setWorkArea(workArea);
+        FH_S.addTask(F);
+        FH_S.addTask(H);
+
+        ParalleFlow DE_P = new ParalleFlow();
+        DE_P.setWorkArea(workArea);
+        DE_P.addTask(D);
+        DE_P.addTask(E);
+
+        SerialFlow DEG_S = new SerialFlow();
+        DEG_S.setWorkArea(workArea);
+        DEG_S.addTask(DE_P);
+        DEG_S.addTask(G);
+
+        ParalleFlow DEG_FH_P = new ParalleFlow();
+        DEG_FH_P.setWorkArea(workArea);
+        DEG_FH_P.addTask(DEG_S);
+        DEG_FH_P.addTask(FH_S);
+
+
+        full.addTask(ABC_S);
+        full.addTask(DEG_FH_P);
+        full.addTask(I);
+        full.addTask(J);
 
         ExecutorService service = Executors.newFixedThreadPool(1);
-        service.submit(flow_1);
+        service.submit(full);
 
-        SleepHelper.sleepSeconds(4);
+        SleepHelper.sleepSeconds(4000);
     }
 
     @Test
