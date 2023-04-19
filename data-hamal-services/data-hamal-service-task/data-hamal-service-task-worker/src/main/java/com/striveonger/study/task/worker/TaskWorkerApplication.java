dfc2f982821351164d0befdc69e5ff817a80893f
Striveonger <striveonger@163.com>
2023-04-19 16:29:38 +0800

    开始写Task吧

--- data-hamal-services/data-hamal-service-task/data-hamal-service-task-worker/src/main/java/com/striveonger/study/task/worker/TaskWorkerApplication.java
+++ data-hamal-services/data-hamal-service-task/data-hamal-service-task-worker/src/main/java/com/striveonger/study/task/worker/TaskWorkerApplication.java
@@ -2,6 +2,7 @@ package com.striveonger.study.task.worker;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -13,6 +14,10 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan({"com.striveonger.study.*"})
 public class TaskWorkerApplication {
-    private final Logger log = LoggerFactory.getLogger(TaskWorkerApplication.class);
+    private static final Logger log = LoggerFactory.getLogger(TaskWorkerApplication.class);
 
+    public static void main(String[] args) {
+        SpringApplication.run(TaskWorkerApplication.class, args);
+        log.info("(♥◠‿◠)ﾉﾞ  data-hamal-service-task-worker start success  ლ(´ڡ`ლ)ﾞ");
+    }
 }
