8d0ddc5e0b49353340ce28f7720bbe2fffc9e888
Striveonger <striveonger@163.com>
2023-07-20 18:30:46 +0800

    重构RuntimeContext RuntimeStatus

--- data-hamal-services/data-hamal-service-task/task-core/src/main/java/com/striveonger/study/task/core/scope/context/storage/ContextStorageHolder.java
+++ data-hamal-services/data-hamal-service-task/task-core/src/main/java/com/striveonger/study/task/core/scope/context/storage/ContextStorageHolder.java
@@ -11,6 +11,23 @@ import org.slf4j.LoggerFactory;
 public class ContextStorageHolder {
     private final Logger log = LoggerFactory.getLogger(ContextStorageHolder.class);
 
+    /*
+    public static class Holder {
+        private static final Holder instance = new Holder();
+
+        private final StatusControls controls;
+
+        private Holder() {
+            ServiceLoader<StatusControls> loader = ServiceLoader.load(StatusControls.class);
+            // 尝试加载外部实现, 如果没有加载到外部实现类, 则使用默认的
+            this.controls  = loader.findFirst().orElseGet(Default::new);
+        }
+
+    public static StatusControls getControls() {
+        return instance.controls;
+    }
+}
+     */
 
 
 }
